a4262d90b931597683074383254faac3b26b2a6e
statuscwo <kevinhattorimusic@gmail.com>
2026-07-18 00:52:14 -0500

    adding team logos

---
+++ src/components/MNTeamLogos.jsx
@@ -0,0 +1,17 @@
+const LOGOS = {
+  wild:         'https://a.espncdn.com/i/teamlogos/nhl/500/min.png',
+  timberwolves: 'https://a.espncdn.com/i/teamlogos/nba/500/min.png',
+  vikings:      'https://a.espncdn.com/i/teamlogos/nfl/500/min.png',
+}
+
+export function WildLogo({ size = 56, className = '' }) {
+  return <img src={LOGOS.wild} width={size} height={size} className={`mn-logo ${className}`} alt="Minnesota Wild" />
+}
+
+export function TimberwolvesLogo({ size = 56, className = '' }) {
+  return <img src={LOGOS.timberwolves} width={size} height={size} className={`mn-logo ${className}`} alt="Minnesota Timberwolves" />
+}
+
+export function VikingsLogo({ size = 56, className = '' }) {
+  return <img src={LOGOS.vikings} width={size} height={size} className={`mn-logo ${className}`} alt="Minnesota Vikings" />
+}
